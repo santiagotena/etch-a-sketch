cc02b550e03f93ce185885cb64a7d4020e871eb7
Santiago Tena Hernandez <stenahe@gmail.com>
2023-02-28 15:05:24 +0100

    Add changeSize func

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function populateBoard(size) {
 	let board = document.querySelector('.board');
+	let squares = board.querySelectorAll('div');
+	squares.forEach((div) => div.remove());
+
 	board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-	// board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+	board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 	
-	for (let i = 0; i < 256; i++) {
+	for (let i = 0; i < (size*size); i++) {
 		let square = document.createElement('div');
 		square.style.backgroundColor = 'blue';
 		board.insertAdjacentElement('beforeend', square);
@@ -11,3 +14,10 @@ function populateBoard(size) {
 }
 
 populateBoard(16);
+
+function changeSize(input) {
+	if (input >= 2 && input <= 100)
+		populateBoard(input);
+	else
+		console.log('Invalid number of squares');
+}
