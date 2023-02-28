b2d930824e9efc42c76c6af8b8e5e4e4a01ee967
Santiago Tena Hernandez <stenahe@gmail.com>
2023-02-28 15:09:11 +0100

    Add color when hovering over effect

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ function populateBoard(size) {
 	let board = document.querySelector('.board');
 	let squares = board.querySelectorAll('div');
 	squares.forEach((div) => div.remove());
-
 	board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
 	board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 	
 	for (let i = 0; i < (size*size); i++) {
 		let square = document.createElement('div');
+		square.addEventListener("mouseover", () => {
+			square.style.backgroundColor = 'black';
+		})
 		square.style.backgroundColor = 'blue';
 		board.insertAdjacentElement('beforeend', square);
 	}
